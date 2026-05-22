c74d21872c570def8d0e2a25fe5450eda57fa6c1
criseri23 <misselb624@gmail.com>
2026-05-21 21:52:48 -0300

    Add files via upload

---
+++ program.cs
@@ -0,0 +1,18 @@
+﻿class Program2
+{
+    static void Main(string[] args)
+    {
+        SmartTV tv1 = new SmartTV("Samsung", 55, true);
+
+        Console.WriteLine(tv1.CODIGO_CONFIG);
+
+        tv1.Power();
+
+        tv1.CambiarCanal();
+        tv1.CambiarCanal(80);
+
+        tv1.RegularVolumen(true);
+
+        tv1.MostrarEstado();
+    }
+}
