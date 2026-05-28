f2f541749389723105b277df2f9f80b95f0eba67
criseri23 <misselb624@gmail.com>
2026-05-28 20:47:41 -0300

    Refactor Automovil class for improved readability

--- automoviles/automovil.cs
+++ automoviles/automovil.cs
@@ -1,21 +1,20 @@
-﻿using System;
+﻿
+using System;
 
 class Automovil
 {
-    private string _marca;
-    private bool _motorEncendido;
-    private int _velocidadActual;
-    private bool _cajaAutomatica;
-    private bool _modoCrucero;
+    private string marca; private bool motorEncendido;
+    private int velocidadActual;
+    private bool cajaAutomatica;
+    private bool modoCrucero;
 
     public Automovil(string marca, bool cajaAutomatica)
     {
-        this._marca = marca;
-        this._cajaAutomatica = cajaAutomatica;
-        _motorEncendido = false;
-        _motorEncendido = false;
-        _velocidadActual = 0;
-        _modoCrucero = false;
+        this.marca = marca;
+        this.cajaAutomatica = cajaAutomatica;
+        motorEncendido = false;
+        velocidadActual = 0;
+        modoCrucero = false;
     }
 
     public string IDENTIFICADOR
@@ -24,73 +23,68 @@ class Automovil
         {
             string tipoCaja;
 
-            if (_cajaAutomatica)
+            if (cajaAutomatica)
             {
                 tipoCaja = "AUTO";
-         }
+            }
             else
             {
                 tipoCaja = "MAN";
             }
 
-            return _marca.Substring(0, 3).ToUpper() + "-" + tipoCaja + "-2026";
+            return marca.Substring(0, 3).ToUpper() + "-" + tipoCaja + "-2026";
         }
     }
 
     public void EncenderApagar()
     {
-        _motorEncendido = !_motorEncendido;
+        motorEncendido = !motorEncendido;
 
-        if (!_motorEncendido)
+        if (!motorEncendido)
         {
-            _velocidadActual = 0;
-            _modoCrucero = false;
+            velocidadActual = 0;
+            modoCrucero = false;
         }
     }
 
     public void Acelerar()
     {
-        if (_motorEncendido)
+        if (motorEncendido)
         {
-            _velocidadActual += 10;
+            velocidadActual += 10;
         }
     }
 
     public void Acelerar(int km)
     {
-        if (_motorEncendido)
+        if (motorEncendido)
         {
-            _velocidadActual += km;
-        }
-        else(
-               !_motorEncendido )
-               {
-            console.Writeline("No se puede acelerar porque el motor esta apagado")
-                return;
+            velocidadActual += km;
         }
     }
-   public void Frenar()
+
+    public void Frenar()
     {
-        _velocidadActual = 0;
-        _modoCrucero = false;
+        velocidadActual = 0;
+        modoCrucero = false;
     }
 
     public void Frenar(int km)
     {
-        _velocidadActual -= km;
+        velocidadActual -= km;
 
-        if (_velocidadActual < 0)
+        if (velocidadActual < 0)
         {
-            _velocidadActual = 0;
+            velocidadActual = 0;
         }
 
-        _modoCrucero = false;
+        modoCrucero = false;
     }
 
     public void MostrarDatos()
     {
-        Console.WriteLine("Motor: " + _motorEncendido);
-        Console.WriteLine("Velocidad: " + _velocidadActual);
-        Console.WriteLine("Modo crucero: " + _modoCrucero);
+        Console.WriteLine("Motor: " + motorEncendido);
+        Console.WriteLine("Velocidad: " + velocidadActual);
+        Console.WriteLine("Modo crucero: " + modoCrucero);
     }
 }
