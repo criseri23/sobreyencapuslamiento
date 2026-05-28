f043e043d98a9521350862a782231fae1c3d23fd
criseri23 <misselb624@gmail.com>
2026-05-28 20:31:18 -0300

    Add files via upload

---
+++ microoo/microondas.cs
@@ -0,0 +1,60 @@
+﻿class Microondas
+{
+   private int potencia;
+    private int tiempoSegundos;
+    private bool puertaAbierta;
+    private bool enFuncionamiento;
+
+    public Microondas(int potencia)
+    {
+     this.potencia = potencia;
+        tiempoSegundos = 0;
+        puertaAbierta = false;
+        enFuncionamiento = false;
+    }
+
+    public string PANTALLA_TIEMPO
+    {
+        get
+        {
+            int minutos = tiempoSegundos / 60;
+            int segundos = tiempoSegundos % 60;
+            return minutos.ToString("00") + ":" + segundos.ToString("00");
+        }
+    }
+
+    public void AgregarTiempo()
+    {
+        tiempoSegundos += 30;
+    }
+
+    public void AgregarTiempo(int segundos)
+    {
+        tiempoSegundos += segundos;
+    }
+
+    public void Iniciar()
+    {
+        if (!puertaAbierta && tiempoSegundos > 0)
+        {
+            enFuncionamiento = true;
+        }
+    }
+
+    public void Detener()
+    {
+        enFuncionamiento = false;
+    }
+
+    public void AbrirCerrarPuerta()
+    {
+        puertaAbierta = !puertaAbierta;
+    }
+
+    public void Mostrar()
+    {
+        Console.WriteLine("Tiempo: " + PANTALLA_TIEMPO);
+        Console.WriteLine("Potencia: " + potencia);
+        Console.WriteLine("Puerta abierta: " + puertaAbierta);
+    }
+}
---
+++ microoo/program.cs
@@ -0,0 +1,14 @@
+﻿class Program3
+{
+    static void Main(string[] args)
+    {
+        Microondas micro1 = new Microondas(8);
+
+        micro1.AgregarTiempo();
+        micro1.AgregarTiempo(90);
+
+        micro1.Iniciar();
+
+        micro1.Mostrar();
+    }
+}
