5ec620f69f2fc9e4d405b347820ddb2c58dfe2a0
criseri23 <misselb624@gmail.com>
2026-06-30 21:51:13 -0300

    Update program.cs

--- program.cs
+++ program.cs
@@ -1,18 +1,60 @@
-﻿class Program2
+﻿using ConsoleApp3;
+using System;
+internal class Program
 {
     static void Main(string[] args)
     {
-        SmartTV tv1 = new SmartTV("Samsung", 55, true);
+        SmartTV tv = new SmartTV("Samsung", 55, true);
 
-        Console.WriteLine(tv1.CODIGO_CONFIG);
+        int opcion;
 
-        tv1.Power();
+        do
+        {
+            Console.Clear();
 
-        tv1.CambiarCanal();
-        tv1.CambiarCanal(80);
+            Console.WriteLine("=== SMART TV ===");
+            Console.WriteLine(tv.CodigoConfig);
+            Console.WriteLine($"Estado: {(tv.Encendido ? "ON" : "OFF")}");
+            Console.WriteLine($"Canal: {tv.CanalActual}");
+            Console.WriteLine($"Volumen: {tv.VolumenTexto}");
 
-        tv1.RegularVolumen(true);
+            Console.WriteLine();
+            Console.WriteLine("1 - Power");
+            Console.WriteLine("2 - Canal +1");
+            Console.WriteLine("3 - Cambiar canal");
+            Console.WriteLine("4 - Volumen +");
+            Console.WriteLine("5 - Volumen -");
+            Console.WriteLine("0 - Salir");
 
-        tv1.MostrarEstado();
+            opcion = int.Parse(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    tv.Power();
+                    break;
+
+                case 2:
+                    tv.CambiarCanal();
+                    break;
+
+                case 3:
+                    Console.Write("Canal: ");
+                    int canal = int.Parse(Console.ReadLine());
+                    tv.CambiarCanal(canal);
+                    break;
+
+                case 4:
+                    tv.RegularVolumen(true);
+                    break;
+
+                case 5:
+                    tv.RegularVolumen(false);
+                    break;
+            }
+
+            Console.ReadLine();
+
+        } while (opcion != 0);
     }
 }
